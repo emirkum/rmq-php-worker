782b5a1f47fcde3311f9d170598202788d810127
Emir Mujic <mujic.emir@nsoft.com>
2018-10-15 22:06:37 +0200

    K8S ready

--- worker.php
+++ worker.php
@@ -6,12 +6,6 @@ require __DIR__ . '/vendor/autoload.php';
 use RMQPHP\App\Exceptions\MissingValidReceiverException;
 use RMQPHP\App\Worker;
 use RMQPHP\App\Worker\Services\PaymentReceiver;
-use josegonzalez\Dotenv\Loader;
-
-$overwrite = true;
-$Loader = (new josegonzalez\Dotenv\Loader(__DIR__ . '/.env'))
-              ->parse()
-              ->putenv($overwriteENV);
 
 $worker = new Worker();
 $paymentReceiver = new PaymentReceiver();
